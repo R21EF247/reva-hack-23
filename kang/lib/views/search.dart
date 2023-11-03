c0f2e2c4dd99fdaf200c9e361b4af4ac324f1a8d
Saravana200 <96806975+Saravana200@users.noreply.github.com>
2023-11-04 04:20:44 +0530

    added gmap and search feature

--- kang/lib/views/search.dart
+++ kang/lib/views/search.dart
@@ -1,21 +1,66 @@
 import 'package:auto_route/annotations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_google_places/flutter_google_places.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_maps_webservice/places.dart';
 
 @RoutePage()
-class SearchPage extends StatelessWidget {
+class SearchPage extends StatefulWidget {
+  @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final theme = Theme.of(context);
+    double mapSize = size.height * 0.78;
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Blue Container Example'),
-      ),
-      body: Center(
-        child: Container(
-          width: 200,
-          height: 200,
-          color: Colors.blue,
-        ),
+      backgroundColor: theme.colorScheme.primary,
+      extendBodyBehindAppBar: true,
+      body: SingleChildScrollView(
+        child: Column(children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 64.0),
+            child: Container(
+              height: size.height * 0.06,
+              width: size.width * 0.84,
+              decoration: BoxDecoration(
+                  color: theme.colorScheme.secondaryContainer,
+                  borderRadius: BorderRadius.circular(12)),
+              child: const TextField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.search),
+                  border: InputBorder.none,
+                  hintText: 'Enter Area',
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: size.height * 0.08,
+          ),
+          AnimatedContainer(
+            height: mapSize,
+            duration: Duration(milliseconds: 100),
+            child: const ClipRRect(
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(34), topRight: Radius.circular(34)),
+              child: Align(
+                child: GoogleMap(
+                  // mapType: MapType.hybrid,
+                  initialCameraPosition: CameraPosition(
+                    target: LatLng(45.521563, -122.677433),
+                    zoom: 11,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ]),
       ),
     );
   }
