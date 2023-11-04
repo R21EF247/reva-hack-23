c0049ff0955b1ebdab26c758b085272428d4b15a
Saravana200 <96806975+Saravana200@users.noreply.github.com>
2023-11-04 10:47:58 +0530

    added profile page

--- kang/lib/views/search.dart
+++ kang/lib/views/search.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:auto_route/annotations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -16,74 +18,106 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  double mapSize = 1;
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     final theme = Theme.of(context);
-    double mapSize = size.height * 0.78;
+
     TextEditingController controller = TextEditingController();
-    onEdit(context);
+
     return Scaffold(
       backgroundColor: theme.colorScheme.primary,
       extendBodyBehindAppBar: true,
       body: SingleChildScrollView(
-        child: Column(children: [
-          Padding(
-            padding: const EdgeInsets.only(top: 64.0),
-            child: Container(
-              height: size.height * 0.06,
-              width: size.width * 0.84,
-              decoration: BoxDecoration(
-                  color: theme.colorScheme.secondaryContainer,
-                  borderRadius: BorderRadius.circular(12)),
-              child: TextFormField(
-                controller: controller,
-                onChanged: (value) => onEdit(context),
-                decoration: const InputDecoration(
-                  prefixIcon: Icon(Icons.search),
-                  border: InputBorder.none,
-                  hintText: 'Enter Area',
-                ),
-              ),
-            ),
-          ),
-          SizedBox(
-            height: size.height * 0.08,
-          ),
-          AnimatedContainer(
-            height: mapSize,
-            duration: Duration(milliseconds: 100),
-            child: const ClipRRect(
-              borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(34), topRight: Radius.circular(34)),
-              child: Align(
-                child: GoogleMap(
-                  // mapType: MapType.hybrid,
-                  initialCameraPosition: CameraPosition(
-                    target: LatLng(45.521563, -122.677433),
-                    zoom: 11,
+        child: SizedBox(
+          height: size.height * 0.98,
+          child: Stack(
+            children: [
+              Positioned(
+                top: size.height * 0.08,
+                left: size.width * 0.07,
+                child: Container(
+                  height: size.height * 0.06,
+                  width: size.width * 0.84,
+                  decoration: BoxDecoration(
+                      color: theme.colorScheme.secondaryContainer,
+                      borderRadius: BorderRadius.circular(12)),
+                  child: Row(
+                    children: [
+                      SizedBox(
+                        height: size.height * 0.06,
+                        width: size.width * 0.70,
+                        child: TextFormField(
+                          controller: controller,
+                          onTap: () => onEdit(context),
+                          decoration: const InputDecoration(
+                            prefixIcon: Icon(Icons.search),
+                            border: InputBorder.none,
+                            hintText: 'Enter Area',
+                          ),
+                        ),
+                      ),
+                      IconButton(
+                          onPressed: () {
+                            setState(() {
+                              mapSize = mapSize == 0.5 ? 1 : 0.5;
+                            });
+                          },
+                          icon: Icon(Icons.tune))
+                    ],
                   ),
                 ),
               ),
-            ),
+              Positioned(
+                bottom: 0,
+                child: AnimatedContainer(
+                  curve: Curves.ease,
+                  width: size.width,
+                  height: size.height * 0.78 * mapSize,
+                  duration: Duration(milliseconds: 100),
+                  child: const ClipRRect(
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(34),
+                        topRight: Radius.circular(34)),
+                    child: Align(
+                      child: GoogleMap(
+                        initialCameraPosition: CameraPosition(
+                          target: LatLng(45.521563, -122.677433),
+                          zoom: 11,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
-        ]),
+        ),
       ),
     );
   }
 
   Future<void> onEdit(BuildContext context) async {
     var kGoogleApiKey = ConstantValues.GmapApiKey();
-    Prediction? p = await PlacesAutocomplete.show(
+    try {
+      Prediction? p = await PlacesAutocomplete.show(
         offset: 0,
         radius: 1000,
         types: [],
         strictbounds: false,
-        region: "ar",
         context: context,
         apiKey: kGoogleApiKey,
         mode: Mode.overlay, // Mode.fullscreen
         language: "en",
-        components: [Component(Component.country, "pk")]);
+        components: [Component(Component.country, "us")],
+      );
+
+      // Handle the response, e.g., update the UI or move to the next screen
+    } catch (e) {
+      // Handle the error, e.g., show a toast or a dialog
+      log("Error occurred: $e");
+    }
   }
 }
